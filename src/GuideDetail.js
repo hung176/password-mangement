17f80f9c83ec97d666e5fde74c167025b4b7fd0f
hung176 <nvhung.bme@gmail.com>
2021-07-09 22:58:19 +0700

    add guide detail page

--- src/GuideDetail.js
+++ src/GuideDetail.js
@@ -1,12 +1,40 @@
-import React from 'react';
-import { Box, Stack } from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box, Stack, IconButton, Text } from '@chakra-ui/react';
+import { EditIcon } from '@chakra-ui/icons';
 import GuideStepHeader from './GuideStepHeader';
 
+import firebase from './firebase';
+
+const db = firebase.firestore();
+
 const GuideDetail = () => {
-  console.log('guide detail')
+  const { unitId, property } = useParams();
+  const [passwords, setPasswords] = useState({});
+  const [guidelines, setGuidelines] = useState({});
+  const [isEditHeader, setIsEditHeader] = useState(false);
+
+  useEffect(() => {
+    const getPasswordsData = async() => {
+      const data = await db.collection('passwords').doc(unitId).get();
+      setPasswords(data.data());
+    };
+
+    getPasswordsData();
+  }, [unitId]);
+
+  useEffect(() => {
+    const getGuidelinesData = async() => {
+      const data = await db.collection('guidelines').doc(unitId).get();
+      setGuidelines(data.data());
+    };
+
+    getGuidelinesData();
+  }, [unitId]);
+
   return (
     <Box
-      p={{ base: 4, md: 6 }}
+      p={{ base: 6, md: 8 }}
       width={{ base: '95%', md: '80%' }}
       borderWidth={2}
       borderRadius={5}
@@ -16,10 +44,37 @@ const GuideDetail = () => {
       mt={4}
       m="20px auto"
     >
-      <Stack spacing={4}>
-        <GuideStepHeader number="1" title="Go to the Config" />
-        <GuideStepHeader number="2" title="Go to the System and ..." />
+      <Stack spacing={0} mb={6}>
+        <Text fontSize="6xl" fontWeight="extrabold" >{unitId}</Text>
+        <Stack isInline spacing={2}>
+          <Text fontSize="2xl">{property}:</Text>
+          <Text fontSize="2xl">{passwords[property]}</Text>
+        </Stack>
       </Stack>
+      <Stack spacing={4}>
+        <GuideStepHeader
+          number="1"
+          value={guidelines['step1'] || {}}
+          isEditHeader={isEditHeader}
+        />
+        <GuideStepHeader
+          number="2"
+          value={guidelines['step1'] || {}}
+          isEditHeader={isEditHeader}
+        />
+      </Stack>
+
+      <IconButton
+        icon={<EditIcon />}
+        fontSize="30px"
+        p={2}
+        variant="ghost"
+        position="absolute"
+        top={2}
+        right={2}
+        onClick={() => setIsEditHeader(true)}
+      />
+
     </Box>
   );
 };
--- src/GuideStepHeader.js
+++ src/GuideStepHeader.js
@@ -2,35 +2,51 @@ import React from 'react';
 import {
   Box,
   Stack,
-  Text,
+  InputGroup,
+  InputRightElement,
+  Input,
 } from '@chakra-ui/react';
+import { CheckIcon } from '@chakra-ui/icons';
 
 const GuideStepHeader = ({
-  number, title, ...rest
-}) => (
-  <Stack isInline align="center" spacing={4} {...rest}>
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      borderRadius="50%"
-      width={10}
-      height={10}
-      border="2px solid"
-      borderColor="gray.200"
-      textAlign="center"
-      lineHeight="100%"
-      fontWeight="bold"
-      color="gray.500"
-      flexGrow={0}
-      flexShrink={0}
-    >
-      {number}
-    </Box>
-    <Text fontSize="lg" fontWeight="medium" isTruncated>
-      {title}
-    </Text>
-  </Stack>
-);
+  number, value, isEditHeader,
+}) => {
+  return (
+    <Stack isInline align="center" spacing={4}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        borderRadius="50%"
+        width={10}
+        height={10}
+        border="2px solid"
+        borderColor="gray.200"
+        textAlign="center"
+        lineHeight="100%"
+        fontWeight="bold"
+        color="gray.500"
+        flexGrow={0}
+        flexShrink={0}
+      >
+        {number}
+      </Box>
+
+      <InputGroup>
+        <Input
+          fontSize="lg"
+          borderStyle={isEditHeader ? '' : 'hidden'}
+          disabled={!isEditHeader}
+          value={value.heading}
+        />
+        <InputRightElement
+          children={<CheckIcon color="green.500" />}
+          display={ isEditHeader ? 'flex' : 'none'}
+          // onClick={handleCheck}
+        />
+      </InputGroup>
+    </Stack>
+  );
+};
 
 export default GuideStepHeader;
